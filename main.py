66fbe1579db4c1da444c39a68a96954970072623
Gnu-Bricoleur <sylvain.migaud@gmail.com>
2016-01-18 22:12:55 +0100

    ajout generation dungeon labyrinthe partiel

--- main.py
+++ main.py
@@ -45,7 +45,21 @@ choix = 0
 #global sauvegarde
 sauvegarde = 0
 modifsasauvegarder = {}
+enregistrementdemodele = False
+positionpremierbloc=(0,0,0)
+modeleenenregistrement = {}
 
+#size of the map du dungeon
+MAP_WIDTH = 80
+MAP_HEIGHT = 45
+ 
+#parameters for dungeon generator
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
+mapdungeon = [[ False
+			for y in range(MAP_HEIGHT) ]
+				for x in range(MAP_WIDTH) ]
 
 def cube_vertices(x, y, z, n):
 	""" Return the vertices of the cube at position x, y, z with size 2*n.
@@ -161,6 +175,8 @@ def sectorize(position):
 
 class Model(object):
 	global modifsasauvegarder
+	global positionpremierbloc
+	global enregistrementdemodele
 	def __init__(self):
 
 		# A Batch is a collection of vertex lists for batched rendering.
@@ -195,6 +211,7 @@ class Model(object):
 		self._initialize()
 
 	def creer_monde(self):
+		global mapdungeon
 		freq = 16.0 * octaves
 		increment=0
 		hauteur=0
@@ -243,6 +260,14 @@ class Model(object):
 					# create outer walls.
 					for dy in xrange(-2, 3):
 						self.add_block((x, y + dy, z), STONE, immediate=False)
+		make_map()
+		print mapdungeon
+		xmax=len(mapdungeon)
+		ymax=len(mapdungeon[0])
+		for xdun in range(xmax):
+			for ydun in range(ymax):
+				if mapdungeon[xdun][ydun]==True:
+					self.add_block((xdun, 21, ydun), STONE, immediate=False)
 
 
 	def biome(self,taillebiome,indexbiome,biomecourant):
@@ -409,6 +434,8 @@ class Model(object):
 		return False
 
 	def add_block(self, position, texture, immediate=True):
+		global positionpremierbloc
+		
 		""" Add a block with the given `texture` and `position` to the world.
 
 		Parameters
@@ -422,6 +449,10 @@ class Model(object):
 			Whether or not to draw the block immediately.
 
 		"""
+		if enregistrementdemodele == True:
+			if modeleenenregistrement == {}:
+				positionpremierbloc=position
+			modeleenenregistrement[(position[0]-positionpremierbloc[0],position[1]-positionpremierbloc[1],position[2]-positionpremierbloc[2])]=texture
 		modifsasauvegarder[position]=texture
 		if position in self.world:
 			self.remove_block(position, immediate)
@@ -652,6 +683,9 @@ class Model(object):
 
 
 class Window(pyglet.window.Window):
+	
+
+	global enregistrementdemodele
 
 	def __init__(self, *args, **kwargs):
 		super(Window, self).__init__(*args, **kwargs)
@@ -954,6 +988,7 @@ class Window(pyglet.window.Window):
 	"""
 
 	def on_key_press(self, symbol, modifiers):
+		global modeleenenregistrement
 		""" Called when the player presses a key. See pyglet docs for key
 		mappings.
 
@@ -965,7 +1000,7 @@ class Window(pyglet.window.Window):
 			Number representing any modifying keys that were pressed.
 
 		"""
-		global sauvegarde
+		global enregistrementdemodele
 		if symbol == key.Z:
 			self.strafe[0] -= 1
 		elif symbol == key.S:
@@ -982,6 +1017,14 @@ class Window(pyglet.window.Window):
 			self.model.save()
 #			picklableMethod = MethodProxy(Model, Model.model)
 #			cpickle.dump( picklableMethod, open( "Sauvegardes/saveWorld.p", "wb" ) )
+		elif symbol == key.N:
+			if enregistrementdemodele == False:
+				enregistrementdemodele = True
+			else :
+				enregistrementdemodele = False
+				positionpremierbloc=(0,0,0)
+				pickle.dump( modeleenenregistrement, open( "Mesmodeles/modele.p", "wb" ) )
+				modeleenenregistrement={}
 		elif symbol == key.SPACE:
 			if self.dy == 0:
 				self.dy = JUMP_SPEED
@@ -1091,11 +1134,19 @@ class Window(pyglet.window.Window):
 		""" Draw the label in the top left of the screen.
 
 		"""
-		x, y, z = self.position
-		self.label.text = '%02d (%.2f, %.2f, %.2f) %d / %d' % (
-			pyglet.clock.get_fps(), x, y, z,
-			len(self.model._shown), len(self.model.world))
-		self.label.draw()
+		global enregistrementdemodele
+		if enregistrementdemodele == False :
+			x, y, z = self.position
+			self.label.text = '%02d (%.2f, %.2f, %.2f) %d / %d' % (
+				pyglet.clock.get_fps(), x, y, z,
+				len(self.model._shown), len(self.model.world))
+			self.label.draw()
+		else :
+			x, y, z = self.position
+			self.label.text = '%02d (%.2f, %.2f, %.2f) %d / %d rec' % (
+				pyglet.clock.get_fps(), x, y, z,
+				len(self.model._shown), len(self.model.world))
+			self.label.draw()
 
 	def draw_reticle(self):
 		""" Draw the crosshairs in the center of the screen.
@@ -1243,6 +1294,103 @@ class MethodProxy(object):
 
 	def __call__(self, *args, **kwargs):
 		return getattr(self.obj, self.methodName)(*args, **kwargs)
+
+class Rect:
+	#a rectangle on the map. used to characterize a room.
+	def __init__(self, x, y, w, h):
+		self.x1 = x
+		self.y1 = y
+		self.x2 = x + w
+		self.y2 = y + h
+ 
+	def center(self):
+		center_x = (self.x1 + self.x2) / 2
+		center_y = (self.y1 + self.y2) / 2
+		return (center_x, center_y)
+ 
+	def intersect(self, other):
+		#returns true if this rectangle intersects with another one
+		return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+				self.y1 <= other.y2 and self.y2 >= other.y1)
+
+
+def create_room(room):
+	global mapdungeon
+	#go through the tiles in the rectangle and make them passable
+	for x in range(room.x1 + 1, room.x2):
+		for y in range(room.y1 + 1, room.y2):
+			mapdungeon[x][y] = True
+ 
+def create_h_tunnel(x1, x2, y):
+	global mapdungeon
+	#horizontal tunnel. min() and max() are used in case x1>x2
+	for x in range(min(x1, x2), max(x1, x2) + 1):
+		mapdungeon[x][y] = True
+ 
+def create_v_tunnel(y1, y2, x):
+	global mapdungeon
+	#vertical tunnel
+	for y in range(min(y1, y2), max(y1, y2) + 1):
+		mapdungeon[x][y] = True
+ 
+def make_map():
+	global mapdungeon, player 
+	#fill map with "blocked" tiles
+	 
+	rooms = []
+	num_rooms = 0
+	
+	for r in range(MAX_ROOMS):
+		#random width and height
+		w = random.randint(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+		h = random.randint(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+		#random position without going out of the boundaries of the map
+		x = random.randint(0, MAP_WIDTH - w - 1)
+		y = random.randint(0, MAP_HEIGHT - h - 1)
+		
+		#"Rect" class makes rectangles easier to work with
+		new_room = Rect(x, y, w, h)
+		
+		#run through the other rooms and see if they intersect with this one
+		failed = False
+		for other_room in rooms:
+			if new_room.intersect(other_room):
+				failed = True
+				break
+		
+		if not failed:
+			#this means there are no intersections, so this room is valid
+			
+			#"paint" it to the map's tiles
+			create_room(new_room)
+			
+			#center coordinates of new room, will be useful later
+			(new_x, new_y) = new_room.center()
+			
+			if num_rooms == 0:
+				#this is the first room, where the player starts at
+				pass
+			else:
+				#all rooms after the first:
+				#connect it to the previous room with a tunnel
+			
+				#center coordinates of previous room
+				(prev_x, prev_y) = rooms[num_rooms-1].center()
+			
+				#draw a coin (random number that is either 0 or 1)
+				if random.randint(0, 1) == 1:
+					#first move horizontally, then vertically
+					create_h_tunnel(prev_x, new_x, prev_y)
+					create_v_tunnel(prev_y, new_y, new_x)
+				else:
+					#first move vertically, then horizontally
+					create_v_tunnel(prev_y, new_y, prev_x)
+					create_h_tunnel(prev_x, new_x, new_y)
+			
+			#finally, append the new room to the list
+			rooms.append(new_room)
+			num_rooms += 1
+
 
 #picklableMethod = MethodProxy(someObj, someObj.method)
 
